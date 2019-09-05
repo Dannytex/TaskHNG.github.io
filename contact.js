581bce93a0a1fc69b2f512c85eb63aece65e940c
[Dannytex] <[edmunddaniels06@gmail.com]>
2019-09-05 12:00:10 +0100

    added new style with js file

---
+++ contact.js
@@ -0,0 +1,46 @@
+ // Form validation code
+function validate()                              
+{ 
+    var name = document.forms["contactform"]["Name"];            
+    var email = document.forms["contactform"]["Email"];  
+    var Title = document.forms["contactform"]["Subject"]; 
+    var message = document.forms["contactform"]["Message"]; 
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (name.value == ""  ||  name.value.length != 4 )
+    { 
+        alert("Please enter your name!"); 
+        name.focus(); 
+        return false; 
+    } 
+    
+    if (email.value.match(mailformat))
+    {
+        Email.focus();
+        return true;
+        }
+        else
+    {
+        alert("You have entered an invalid email address!");
+        Email.focus();
+        return false;
+    }
+
+    if (subject.value == "")
+    {
+        alert("Please enter a Title!"); 
+        subject.focus(); 
+        return false; 
+    }
+
+    if (message.value == ""  ||
+            message.value.length != 20 )                                
+    { 
+        alert("Please enter a message."); 
+        message.focus(); 
+        return false; 
+    }
+    return true; 
+}
+
+//-->
